9c1656b2ea7a826504e732756abea25369f31cb3
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-11-09 01:47:02 +0800

    feat: refactor catppuccin for nvim (#146)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,54 +11,25 @@ return {
 				light = "latte",
 				dark = "macchiato",
 			},
-			transparent_background = false, -- disables setting the background color.
-			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = true, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
-			},
-			no_italic = false, -- Force no italic
-			no_bold = false, -- Force no bold
-			no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				comments = { "italic" }, -- Change the style of comments
-				conditionals = { "italic" },
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
-			},
-			color_overrides = {},
 			custom_highlights = function(colors)
 				return {
 					HighlightUndo = { bg = colors.red, fg = colors.base },
 				}
 			end,
 			integrations = {
-				aerial = true,
-				alpha = true,
+				blink_cmp = true,
 				cmp = true,
 				dashboard = true,
-				blink_cmp = true,
 				flash = true,
 				fzf = true,
 				gitsigns = true,
 				grug_far = true,
+				harpoon = true,
 				headlines = true,
-				illuminate = true,
 				indent_blankline = { enabled = true },
-				leap = true,
 				lsp_trouble = true,
-				mason = true,
 				markdown = true,
+				mason = true,
 				mini = {
 					enabled = true,
 				},
@@ -71,12 +42,9 @@ return {
 						information = { "undercurl" },
 					},
 				},
-				navic = { enabled = true, custom_bg = "lualine" },
-				-- neotree = true,
 				noice = true,
 				notify = true,
-				semantic_tokens = true,
-				telescope = true,
+				render_markdown = true,
 				treesitter = true,
 				treesitter_context = true,
 				which_key = true,
