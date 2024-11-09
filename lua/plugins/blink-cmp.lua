2900832db2a1cafb31a2785bc04b67f1fed5b455
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-11-09 11:51:36 +0800

    feat: update blink keymap to retain tab selection (#148)

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,6 +3,22 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
+		keymap = {
+			preset = "default",
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.is_in_snippet() then
+						return cmp.accept()
+					else
+						return cmp.select_next()
+					end
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+		},
 		windows = {
 			autocomplete = {
 				border = "rounded",
