8a50024548c3ad8d10b24a66fa554e4259db5a20
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-11-10 01:13:08 +0800

    feat: remove included defaults for catppuccin nvim (#150)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,6 @@
 return {
 	{
 		"catppuccin/nvim",
-		lazy = true,
-		name = "catppuccin",
 		---@module 'catppuccin'
 		---@type CatppuccinOptions
 		opts = {
@@ -17,37 +15,7 @@ return {
 				}
 			end,
 			integrations = {
-				blink_cmp = true,
-				cmp = true,
-				dashboard = true,
-				flash = true,
 				fzf = true,
-				gitsigns = true,
-				grug_far = true,
-				harpoon = true,
-				headlines = true,
-				indent_blankline = { enabled = true },
-				lsp_trouble = true,
-				markdown = true,
-				mason = true,
-				mini = {
-					enabled = true,
-				},
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
-				noice = true,
-				notify = true,
-				render_markdown = true,
-				treesitter = true,
-				treesitter_context = true,
-				which_key = true,
 			},
 		},
 	},
