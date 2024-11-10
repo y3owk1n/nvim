594df43678c0af4c1560300069d5743b978c41e7
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-11-10 18:13:27 +0800

    feat: cleanup nvim (#157)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,7 +13,6 @@ return {
 			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
 			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
 			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 		},
 	},
 	{
@@ -37,20 +36,4 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		opts = {},
 	},
-	-- {
-	-- 	"prochri/telescope-all-recent.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-telescope/telescope.nvim",
-	-- 		"kkharji/sqlite.lua",
-	-- 		-- optional, if using telescope for vim.ui.select
-	-- 		"stevearc/dressing.nvim",
-	-- 	},
-	-- 	-- can reset the database by deleting ~/.local/share/nvim/telescope-all-recent.sqlite3
-	-- 	opts = {
-	-- 		-- your config goes here
-	-- 		default = {
-	-- 			sorting = "frecency", -- sorting: options: 'recent' and 'frecency'
-	-- 		},
-	-- 	},
-	-- },
 }
