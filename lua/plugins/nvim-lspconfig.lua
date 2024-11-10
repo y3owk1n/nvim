b34231d38e9c18274728c80e327b6f5f8cd01a63
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-11-10 15:40:32 +0800

    feat: add border for lsp diagnostic (#155)

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,11 @@ return {
 	-- other settings removed for brevity
 	---@class PluginLspOpts
 	opts = {
+		diagnostics = {
+			float = {
+				border = "rounded",
+			},
+		},
 		servers = {
 			biome = {},
 		},
