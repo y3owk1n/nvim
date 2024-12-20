77b107935c0b262c010e4940371b453986b7abb5
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-12-20 08:36:15 +0800

    feat: toggle cursor line in windows automatically (#244)

--- lua/k92/autocmds.lua
+++ lua/k92/autocmds.lua
@@ -167,4 +167,22 @@ vim.api.nvim_create_autocmd("BufNew", {
 	end,
 })
 
+-- show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+	callback = function()
+		if vim.w.auto_cursorline then
+			vim.wo.cursorline = true
+			vim.w.auto_cursorline = nil
+		end
+	end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+	callback = function()
+		if vim.wo.cursorline then
+			vim.w.auto_cursorline = true
+			vim.wo.cursorline = false
+		end
+	end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
