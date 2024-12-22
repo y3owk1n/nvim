9823ad835bff2f93bc439efea36685806a0e00c7
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-12-22 18:18:25 +0800

    chore: remove duplicated I shormess (#264)

--- lua/k92/options.lua
+++ lua/k92/options.lua
@@ -91,7 +91,6 @@ vim.opt.formatoptions:append({ "r" })
 
 vim.opt.spelllang:append("cjk") -- disable spellchecking for asian characters (VIM algorithm does not support it)
 vim.opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
-vim.opt.shortmess:append("I") -- don't show the default intro message
 vim.opt.shortmess:append("A") -- Ignore swap file messages
 vim.opt.shortmess:append({ s = true, I = true }) -- disable search count wrap and startup messages
 
