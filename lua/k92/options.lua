5256520b029e7b0af1100cf4f9b365b1230e3a9b
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-12-22 18:55:10 +0800

    feat: update some opts for nvim (#266)

--- lua/k92/options.lua
+++ lua/k92/options.lua
@@ -1,3 +1,5 @@
+vim.opt.termguicolors = true
+
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -9,6 +11,8 @@ vim.opt.colorcolumn = "120" -- make width to 80
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
+
+vim.opt.ruler = false -- Don't show cursor position in command line
 
 -- Sync clipboard between OS and Neovim.
 vim.schedule(function()
@@ -25,6 +29,8 @@ vim.opt.autoindent = true -- copy indent from current line when starting new one
 -- Enable break indent
 vim.opt.breakindent = true
 
+vim.opt.smartindent = true -- Make indenting smart
+
 -- Save undo history
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
@@ -38,6 +44,7 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.infercase = true -- Infer letter cases for a richer built-in keyword completion
 vim.opt.hlsearch = false -- do not highlight search
 vim.opt.incsearch = true -- follow the searches
 
@@ -52,6 +59,8 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.splitkeep = "screen" -- Reduce scroll during window split
+
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
@@ -60,6 +69,8 @@ vim.opt.cursorline = true
 
 vim.opt.wrap = false
 
+vim.opt.linebreak = true -- Wrap long lines at 'breakat' (if 'wrap' is set)
+
 -- Minimal number of screen lines to keep above and below the cursor.
 -- vim.opt.scrolloff = 8
 
@@ -67,6 +78,8 @@ vim.opt.wrap = false
 vim.opt.showtabline = 0
 
 vim.opt.synmaxcol = 300 -- Don't syntax highlight long lines
+
+vim.opt.virtualedit = "block" -- Allow going past the end of line in visual block mode
 
 -- words
 vim.opt.path:append("**")
@@ -92,6 +105,7 @@ vim.opt.formatoptions:append({ "r" })
 vim.opt.spelllang:append("cjk") -- disable spellchecking for asian characters (VIM algorithm does not support it)
 vim.opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
 vim.opt.shortmess:append("A") -- Ignore swap file messages
+vim.opt.shortmess:append("WcC") -- Reduce command line messages
 vim.opt.shortmess:append({ s = true, I = true }) -- disable search count wrap and startup messages
 
 vim.opt.viewoptions:remove("curdir") -- disable saving current directory with views
@@ -99,3 +113,10 @@ vim.opt.viewoptions:remove("curdir") -- disable saving current directory with vi
 vim.opt.backspace:append({ "nostop" }) -- don't stop backspace at insert
 
 vim.opt.whichwrap:append("<,>,[,],h,l")
+
+vim.cmd("filetype plugin indent on") -- Enable all filetype plugins
+
+-- Enable syntax highlighting if it wasn't already (as it is time consuming)
+if vim.fn.exists("syntax_on") ~= 1 then
+	vim.cmd([[syntax enable]])
+end
