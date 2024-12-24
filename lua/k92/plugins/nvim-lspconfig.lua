7d00d9cb1d62e067b83507f8e591efac9057fa21
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-12-25 01:25:56 +0800

    feat: remove deprecated fieldalignment from gopls (#286)

--- lua/k92/plugins/nvim-lspconfig.lua
+++ lua/k92/plugins/nvim-lspconfig.lua
@@ -123,7 +123,6 @@ return {
 							rangeVariableTypes = true,
 						},
 						analyses = {
-							fieldalignment = true,
 							nilness = true,
 							unusedparams = true,
 							unusedwrite = true,
