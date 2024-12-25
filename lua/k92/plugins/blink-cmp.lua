7e30c982a7d105912b381180e1a6caec8358b578
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-12-25 13:02:30 +0800

    feat: update blink configurations (#288)

--- lua/k92/plugins/blink-cmp.lua
+++ lua/k92/plugins/blink-cmp.lua
@@ -5,7 +5,7 @@ end
 
 return {
 	"saghen/blink.cmp",
-	event = "VeryLazy",
+	event = "InsertEnter",
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
 	---@module 'blink.cmp'
@@ -49,11 +49,7 @@ return {
 		},
 
 		appearance = {
-			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
-			kind_icons = {
-				Color = "██", -- Use block instead of icon for color items to make swatches more usable
-			},
 		},
 
 		sources = {
@@ -75,33 +71,21 @@ return {
 			menu = {
 				draw = {
 					treesitter = { "lsp" },
-					components = {
-						kind_icon = {
-							ellipsis = false,
-							text = function(ctx)
-								local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-								return kind_icon
-							end,
-							-- Optionally, you may also use the highlights from mini.icons
-							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-								return hl
-							end,
-						},
-					},
 				},
 				border = "rounded",
-				winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
 			},
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 200,
 				window = {
 					border = "rounded",
-					winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
 				},
 			},
 		},
+
+		signature = { enabled = true, window = {
+			border = "rounded",
+		} },
 	},
 	-- allows extending the providers array elsewhere in your config
 	-- without having to redefine it
--- lua/k92/plugins/catppuccin.lua
+++ lua/k92/plugins/catppuccin.lua
@@ -7,8 +7,17 @@ return {
 	opts = {
 		custom_highlights = function(colors)
 			return {
+				-- highlight undo
 				HighlightUndo = { bg = colors.red, fg = colors.base },
 				HighlightRedo = { bg = colors.flamingo, fg = colors.base },
+				-- blink extended
+				-- TODO: Remove this when merged -> https://github.com/catppuccin/nvim/pull/809/files
+				BlinkCmpKind = { fg = colors.blue },
+				BlinkCmpMenu = { fg = colors.text },
+				BlinkCmpMenuBorder = { fg = colors.blue },
+				BlinkCmpDocBorder = { fg = colors.blue },
+				BlinkCmpSignatureHelpBorder = { fg = colors.blue },
+				BlinkCmpSignatureHelpActiveParameter = { bg = colors.mauve, fg = colors.crust },
 			}
 		end,
 		integrations = {
