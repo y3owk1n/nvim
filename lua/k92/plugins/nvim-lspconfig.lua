95c58b3a4fe0cd4a989fe2712f8656108304feb8
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-01-03 03:58:52 +0800

    feat(nvim): use fzf code actions (#377)

--- lua/k92/plugins/nvim-lspconfig.lua
+++ lua/k92/plugins/nvim-lspconfig.lua
@@ -304,7 +304,8 @@ return {
 
 				-- Execute a code action, usually your cursor needs to be on top of an error
 				-- or a suggestion from your LSP for this to activate.
-				map("<leader>ca", vim.lsp.buf.code_action, "Code actions", { "n", "x" })
+				map("<leader>ca", fzf.lsp_code_actions, "Code actions", { "n", "x" })
+				-- map("<leader>ca", vim.lsp.buf.code_action, "Code actions", { "n", "x" })
 
 				-- WARN: This is not Goto Definition, this is Goto Declaration.
 				--  For example, in C this would take you to the header.
