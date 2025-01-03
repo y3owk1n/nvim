3765653ec5c4854a000b6c1466161ef355d07eb2
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-01-04 02:33:31 +0800

    feat: modularise tailwind-autosort (#403)

--- lua/k92/plugins/languages/tailwindcss.lua
+++ lua/k92/plugins/languages/tailwindcss.lua
@@ -14,4 +14,11 @@ return {
 			},
 		},
 	},
+	{
+		"y3owk1n/tailwind-autosort.nvim",
+		-- dir = "~/Dev/tailwind-autosort.nvim", -- Your path
+		event = { "LspAttach" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {},
+	},
 }
--- lua/k92/plugins/tailwind-autosort.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-	"y3owk1n/tailwind-autosort.nvim",
-	-- dir = "~/Dev/tailwind-autosort.nvim", -- Your path
-	event = { "LspAttach" },
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	opts = {},
-}
