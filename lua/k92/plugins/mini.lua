471e5e3c3fe06778988618af1373704720c393aa
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-01-04 02:29:00 +0800

    feat: combine mini modules into single file (#401)

--- lua/k92/plugins/mini.lua
+++ lua/k92/plugins/mini.lua
@@ -1,5 +1,37 @@
----@type LazySpec
 return {
+	{
+		"echasnovski/mini.ai",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			n_lines = 500,
+		},
+	},
+	{
+		"echasnovski/mini.comment",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
+		opts = {
+			custom_commentstring = function()
+				return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+			end,
+		},
+	},
+	{
+		"echasnovski/mini.diff",
+		version = false,
+		event = "VeryLazy",
+		opts = {
+			view = {
+				style = "sign",
+				signs = {
+					add = "▎",
+					change = "▎",
+					delete = "",
+				},
+			},
+		},
+	},
+
 	{
 		"echasnovski/mini.files",
 		event = "VeryLazy",
@@ -131,6 +163,62 @@ return {
 				end,
 			})
 		end,
+	},
+	{
+		"echasnovski/mini-git",
+		event = "VeryLazy",
+		version = false,
+		main = "mini.git",
+		opts = {},
+	},
+	{
+		"echasnovski/mini.icons",
+		event = "VeryLazy",
+		opts = {
+			file = {
+				[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+				["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+			},
+			filetype = {
+				dotenv = { glyph = "", hl = "MiniIconsYellow" },
+			},
+		},
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		event = { "InsertEnter" },
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+	},
+	{
+		"echasnovski/mini.surround",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+			},
+		},
 	},
 	{
 		"catppuccin/nvim",
