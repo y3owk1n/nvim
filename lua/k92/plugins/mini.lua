b03952552fbb2e4df062ec44576154105334b8e8
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-01-08 19:08:21 +0800

    feat(nvim): use ts-comment instead (#446)

--- lua/k92/plugins/mini.lua
+++ lua/k92/plugins/mini.lua
@@ -8,16 +8,6 @@ return {
 		},
 	},
 	{
-		"echasnovski/mini.comment",
-		event = { "VeryLazy" },
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
-		opts = {
-			custom_commentstring = function()
-				return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
-			end,
-		},
-	},
-	{
 		"echasnovski/mini.files",
 		event = "VeryLazy",
 		opts = {
