c3a3b49068bda64e45a213d2b974329cec6c7d93
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-02-05 16:18:12 +0800

    feat(nvim): tailwind lsp support workaround for v3 and v4 + monorepo (#492)

--- lua/k92/plugins/languages/tailwindcss.lua
+++ lua/k92/plugins/languages/tailwindcss.lua
@@ -1,3 +1,20 @@
+local function decode_json_file(filename)
+	local file = io.open(filename, "r")
+	if file then
+		local content = file:read("*all")
+		file:close()
+
+		local ok, data = pcall(vim.fn.json_decode, content)
+		if ok and type(data) == "table" then
+			return data
+		end
+	end
+end
+
+local function has_nested_key(json, ...)
+	return vim.tbl_get(json, ...) ~= nil
+end
+
 ---@type LazySpec
 return {
 	{
@@ -7,18 +24,33 @@ return {
 			opts.servers = opts.servers or {}
 			opts.servers.tailwindcss = function() end
 			opts.servers.tailwindcss = {
-				--- NOTE: Override unique root pattern to just root .git
-				--- If not, it wont work for monorepo
-				--- Since v4 does not have any config file anymore, looking for the git root might be a better
-				--- choice, thought no idea what are the consequences yet...
-				root_dir = root_pattern(
-					-- "tailwind.config.js",
-					-- "tailwind.config.ts",
-					-- "postcss.config.js",
-					-- "package.json",
-					-- "node_modules",
-					".git"
-				),
+				-- NOTE: Override unique root pattern with custom checks
+				-- Current state, it doesn't work for monorepo, since v4 does not have any config anymore
+				-- To cover the use cases of v3 and v4, the following checks are being made for now with an
+				-- assumption of if package.json has tailwindcss installed, the LSP should be activated
+				--   1. Get the git root (as workspace root)
+				--   2. Get the nearest package.json (relative to current file)
+				--   3. Try to find if there's any tailwindcss package installed in the package.json
+				--   4. If yes, then return the git root (so that it can detect nested css from other packages within
+				--      the workspace)
+				root_dir = function(fname)
+					local git_root = root_pattern(".git")(fname)
+
+					local package_root = root_pattern("package.json")(fname)
+
+					if package_root and git_root then
+						local package_data = decode_json_file(package_root .. "/package.json")
+						if
+							package_data
+							and (
+								has_nested_key(package_data, "dependencies", "tailwindcss")
+								or has_nested_key(package_data, "devDependencies", "tailwindcss")
+							)
+						then
+							return git_root
+						end
+					end
+				end,
 			}
 		end,
 	},
