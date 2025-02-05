79dccc14462d2ae2fce12ede421d0649a691107a
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-02-05 11:10:03 +0800

    feat(nvim): search for git root for tailwindcss lsp (fix for monorepo and v4) (#491)

--- lua/k92/plugins/languages/tailwindcss.lua
+++ lua/k92/plugins/languages/tailwindcss.lua
@@ -3,8 +3,23 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		opts = function(_, opts)
+			local root_pattern = require("lspconfig.util").root_pattern
 			opts.servers = opts.servers or {}
-			opts.servers.tailwindcss = {}
+			opts.servers.tailwindcss = function() end
+			opts.servers.tailwindcss = {
+				--- NOTE: Override unique root pattern to just root .git
+				--- If not, it wont work for monorepo
+				--- Since v4 does not have any config file anymore, looking for the git root might be a better
+				--- choice, thought no idea what are the consequences yet...
+				root_dir = root_pattern(
+					-- "tailwind.config.js",
+					-- "tailwind.config.ts",
+					-- "postcss.config.js",
+					-- "package.json",
+					-- "node_modules",
+					".git"
+				),
+			}
 		end,
 	},
 	{
