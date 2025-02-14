ecbfceb5018753d8a3ec8db7fdfadce3cddae2f0
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-02-14 23:36:05 +0800

    feat(nvim): better catppuccin integration with undo-glow.nvim (#524)

--- lua/k92/plugins/undo-glow.lua
+++ lua/k92/plugins/undo-glow.lua
@@ -4,28 +4,26 @@ return {
 		"y3owk1n/undo-glow.nvim",
 		-- dir = "~/Dev/undo-glow.nvim", -- Your path
 		event = { "VeryLazy" },
-		opts = {},
+		---@param _ any
+		---@param opts UndoGlow.Config
+		opts = function(_, opts)
+			local has_catppuccin, catppuccin = pcall(require, "catppuccin.palettes")
+
+			if has_catppuccin then
+				local colors = catppuccin.get_palette()
+				opts.undo_hl_color = { bg = colors.red, fg = colors.base }
+				opts.redo_hl_color = { bg = colors.flamingo, fg = colors.base }
+			end
+		end,
+		---@param _ any
+		---@param opts UndoGlow.Config
 		config = function(_, opts)
 			local undo_glow = require("undo-glow")
+
 			undo_glow.setup(opts)
 
 			vim.keymap.set("n", "u", undo_glow.undo, { noremap = true, silent = true })
 			vim.keymap.set("n", "U", undo_glow.redo, { noremap = true, silent = true })
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		optional = true,
-		opts = function(_, opts)
-			local colors = require("catppuccin.palettes").get_palette()
-			local highlights = {
-				UgUndo = { bg = colors.red, fg = colors.base },
-				UgRedo = { bg = colors.flamingo, fg = colors.base },
-			}
-			opts.custom_highlights = opts.custom_highlights or {}
-			for key, value in pairs(highlights) do
-				opts.custom_highlights[key] = value
-			end
 		end,
 	},
 }
