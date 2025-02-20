6f3ff80acb6edeb99afbf2b1d8f4265e0157af11
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-02-20 12:34:11 +0800

    feat: update undo-glow.nvim opts (#540)

--- lua/k92/plugins/undo-glow.lua
+++ lua/k92/plugins/undo-glow.lua
@@ -4,17 +4,12 @@ return {
 		"y3owk1n/undo-glow.nvim",
 		-- dir = "~/Dev/undo-glow.nvim", -- Your path
 		event = { "VeryLazy" },
-		---@param _ any
-		---@param opts UndoGlow.Config
-		opts = function(_, opts)
-			local has_catppuccin, catppuccin = pcall(require, "catppuccin.palettes")
-
-			if has_catppuccin then
-				local colors = catppuccin.get_palette()
-				opts.undo_hl_color = { bg = colors.maroon }
-				opts.redo_hl_color = { bg = colors.teal }
-			end
-		end,
+		---@type UndoGlow.Config
+		opts = {
+			undo_hl = "DiffDelete",
+			redo_hl = "DiffAdd",
+			duration = 1000,
+		},
 		---@param _ any
 		---@param opts UndoGlow.Config
 		config = function(_, opts)
@@ -32,7 +27,7 @@ return {
 					end
 
 					vim.schedule(function()
-						require("undo-glow").attach_and_run({
+						undo_glow.attach_and_run({
 							hlgroup = "UgUndo",
 						})
 					end)
