dd04abce5e6d68cd6b5767ee99ef309f58c0c05c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-02-21 23:46:54 +0800

    feat: update undo-glow with new cmds (#543)

--- lua/k92/plugins/undo-glow.lua
+++ lua/k92/plugins/undo-glow.lua
@@ -1,8 +1,8 @@
 ---@type LazySpec
 return {
 	{
-		-- "y3owk1n/undo-glow.nvim",
-		dir = "~/Dev/undo-glow.nvim", -- Your path
+		"y3owk1n/undo-glow.nvim",
+		-- dir = "~/Dev/undo-glow.nvim", -- Your path
 		event = { "VeryLazy" },
 		---@type UndoGlow.Config
 		opts = {
@@ -16,18 +16,15 @@ return {
 				yank = {
 					hl_color = { bg = "#5A513C" },
 				},
-				paste_below = {
-					hl_color = { bg = "#5A496E" },
-				},
-				paste_above = {
+				search = {
 					hl_color = { bg = "#6D4B5E" },
 				},
-				search_next = {
+				comment = {
 					hl_color = { bg = "#6D5640" },
 				},
-				search_prev = {
-					hl_color = { bg = "#3E4C63" },
-				},
+				-- search_prev = {
+				-- 	hl_color = { bg = "#3E4C63" },
+				-- },
 			},
 		},
 		---@param _ any
@@ -37,73 +34,38 @@ return {
 
 			undo_glow.setup(opts)
 
-			vim.keymap.set("n", "u", require("undo-glow").undo, { noremap = true, silent = true })
-			vim.keymap.set("n", "U", require("undo-glow").redo, { noremap = true, silent = true })
-			vim.keymap.set("n", "p", require("undo-glow").paste_below, { noremap = true, silent = true })
-			vim.keymap.set("n", "P", require("undo-glow").paste_above, { noremap = true, silent = true })
-			vim.keymap.set("n", "n", require("undo-glow").search_next, { noremap = true, silent = true })
-			vim.keymap.set("n", "N", require("undo-glow").search_prev, { noremap = true, silent = true })
+			vim.keymap.set("n", "u", undo_glow.undo, { noremap = true, desc = "Undo with highlight" })
+			vim.keymap.set("n", "U", undo_glow.redo, { noremap = true, desc = "Redo with highlight" })
+			vim.keymap.set("n", "p", undo_glow.paste_below, { noremap = true, desc = "Paste below with highlight" })
+			vim.keymap.set("n", "P", undo_glow.paste_above, { noremap = true, desc = "Paste above with highlight" })
+			vim.keymap.set("n", "n", undo_glow.search_next, { noremap = true, desc = "Search next with highlight" })
+			vim.keymap.set("n", "N", undo_glow.search_prev, { noremap = true, desc = "Search previous with highlight" })
+			vim.keymap.set("n", "*", undo_glow.search_star, { noremap = true, desc = "Search * with highlight" })
+			vim.keymap.set(
+				{ "n", "x" },
+				"gc",
+				undo_glow.comment,
+				{ expr = true, noremap = true, desc = "Toggle comment with highlight" }
+			)
+
+			vim.keymap.set(
+				"o",
+				"gc",
+				undo_glow.comment_textobject,
+				{ noremap = true, desc = "Comment textobject with highlight" }
+			)
+
+			vim.keymap.set(
+				"n",
+				"gcc",
+				undo_glow.comment_line,
+				{ expr = true, noremap = true, desc = "Toggle comment line with highlight" }
+			)
 
 			vim.api.nvim_create_autocmd("TextYankPost", {
 				desc = "Highlight when yanking (copying) text",
 				callback = require("undo-glow").yank,
 			})
-
-			vim.keymap.set({ "n", "x" }, "gc", function()
-				require("undo-glow").highlight_changes({
-					hlgroup = "UgUndo",
-				})
-				return require("vim._comment").operator()
-			end, { expr = true, desc = "Toggle comment highlight" })
-
-			vim.keymap.set("o", "gc", function()
-				require("undo-glow").highlight_changes({
-					hlgroup = "UgUndo",
-				})
-				return require("vim._comment").textobject()
-			end, { desc = "Comment textobject with highlight" })
-
-			vim.keymap.set({ "n" }, "gcc", function()
-				require("undo-glow").highlight_changes({
-					hlgroup = "UgUndo",
-				})
-				return require("vim._comment").operator() .. "_"
-			end, { expr = true, desc = "Toggle comment line with highlight" })
-
-			vim.keymap.set("n", "*", function()
-				vim.cmd("normal! *")
-
-				local bufnr = vim.api.nvim_get_current_buf()
-				local cursor = vim.api.nvim_win_get_cursor(0)
-				local row = cursor[1] - 1
-
-				local search_pattern = vim.fn.getreg("/")
-				if search_pattern == "" then
-					return
-				end
-
-				local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false)[1]
-				if not line then
-					return
-				end
-
-				local reg = vim.regex(search_pattern)
-				local match_start = reg:match_str(line)
-				if match_start == nil then
-					return
-				end
-
-				local matched_text = vim.fn.matchstr(line, search_pattern)
-				local match_end = match_start + #matched_text
-
-				require("undo-glow").highlight_region({
-					hlgroup = "UgUndo",
-					s_row = row,
-					s_col = match_start,
-					e_row = row,
-					e_col = match_end,
-				})
-			end, { silent = true })
 		end,
 	},
 }
