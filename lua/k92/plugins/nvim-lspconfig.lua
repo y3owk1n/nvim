8d84e0d354447bd222a74bc4b1d9f1c6efe5a0cc
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-02-28 12:36:20 +0800

    feat: remove lsp and snacks.word highlights (#556)

--- lua/k92/plugins/nvim-lspconfig.lua
+++ lua/k92/plugins/nvim-lspconfig.lua
@@ -68,50 +68,7 @@ return {
 						additional_keymap(map)
 					end
 
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					local has_support_document_highlight
-
-					-- TODO: Can remove this on 0.11
-					-- 0.11 -> client:supports_method
-					-- 0.10 -> client.supports_method
-					if vim.fn.has("nvim-0.11") == 1 then
-						has_support_document_highlight = client
-							and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
-					else
-						has_support_document_highlight = client
-							and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
-					end
-
-					if has_support_document_highlight then
-						local highlight_augroup = vim.api.nvim_create_augroup("k92-lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("k92-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({
-									group = "k92-lsp-highlight",
-									buffer = event2.buf,
-								})
-							end,
-						})
-					end
 
 					for _, func in pairs(opts.additional_setup) do
 						func(client)
--- lua/k92/plugins/snacks.lua
+++ lua/k92/plugins/snacks.lua
@@ -393,32 +393,6 @@ return {
 			},
 		},
 	},
-	--- words
-	{
-		"folke/snacks.nvim",
-		---@type snacks.Config
-		opts = {
-			words = { enabled = true },
-		},
-		keys = {
-			{
-				"]]",
-				function()
-					Snacks.words.jump(vim.v.count1)
-				end,
-				desc = "Next Reference",
-				mode = { "n", "t" },
-			},
-			{
-				"[[",
-				function()
-					Snacks.words.jump(-vim.v.count1)
-				end,
-				desc = "Prev Reference",
-				mode = { "n", "t" },
-			},
-		},
-	},
 	--- dashboard
 	{
 		"folke/snacks.nvim",
