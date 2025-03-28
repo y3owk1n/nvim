bcb6bc7825ee41f93cb8b7cf7a3a5a3685ea9061
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-03-29 00:49:33 +0800

    feat(nvim.lsp.vtsls): improve `vtsls` keymaps and capabilities (#628)

--- lsp/vts_ls.lua
+++ lsp/vts_ls.lua
@@ -42,4 +42,84 @@ return {
 			},
 		},
 	},
+	on_attach = function(client, bufnr)
+		local lsp = {}
+		lsp.map = require("k92.utils.lsp").map
+		lsp.execute = require("k92.utils.lsp").execute
+		lsp.action = require("k92.utils.lsp").action
+
+		lsp.map(bufnr, "gD", function()
+			local params = vim.lsp.util.make_position_params(0, "utf-8")
+			lsp.execute({
+				command = "typescript.goToSourceDefinition",
+				arguments = { params.textDocument.uri, params.position },
+				open = true,
+			})
+		end, "Goto Source Definition")
+
+		lsp.map(bufnr, "gR", function()
+			lsp.execute({
+				command = "typescript.findAllFileReferences",
+				arguments = { vim.uri_from_bufnr(0) },
+				open = true,
+			})
+		end, "File References")
+
+		lsp.map(bufnr, "<leader>co", lsp.action["source.organizeImports"], "Organize Imports")
+		lsp.map(bufnr, "<leader>cM", lsp.action["source.addMissingImports.ts"], "Add Missing Imports")
+		lsp.map(bufnr, "<leader>cu", lsp.action["source.removeUnused.ts"], "Remove Unused Imports")
+		lsp.map(bufnr, "<leader>cD", lsp.action["source.fixAll.ts"], "Fix All Diagnostics")
+		lsp.map(bufnr, "cV", function()
+			lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+		end, "Select TS workspace version")
+
+		client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+			---@type string, string, lsp.Range
+			local action, uri, range = unpack(command.arguments)
+
+			local function move(newf)
+				client:request("workspace/executeCommand", {
+					command = command.command,
+					arguments = { action, uri, range, newf },
+				})
+			end
+
+			local fname = vim.uri_to_fname(uri)
+			client:request("workspace/executeCommand", {
+				command = "typescript.tsserverRequest",
+				arguments = {
+					"getMoveToRefactoringFileSuggestions",
+					{
+						file = fname,
+						startLine = range.start.line + 1,
+						startOffset = range.start.character + 1,
+						endLine = range["end"].line + 1,
+						endOffset = range["end"].character + 1,
+					},
+				},
+			}, function(_, result)
+				---@type string[]
+				local files = result.body.files
+				table.insert(files, 1, "Enter new path...")
+				vim.ui.select(files, {
+					prompt = "Select move destination:",
+					format_item = function(f)
+						return vim.fn.fnamemodify(f, ":~:.")
+					end,
+				}, function(f)
+					if f and f:find("^Enter new path") then
+						vim.ui.input({
+							prompt = "Enter move destination:",
+							default = vim.fn.fnamemodify(fname, ":h") .. "/",
+							completion = "file",
+						}, function(newf)
+							return newf and move(newf)
+						end)
+					elseif f then
+						move(f)
+					end
+				end)
+			end)
+		end
+	end,
 }
--- lua/k92/utils/lsp.lua
+++ lua/k92/utils/lsp.lua
@@ -132,6 +132,40 @@ function M.run_async_job(cmd, on_done, on_error)
 	end
 end
 
+---@class LspCommand: lsp.ExecuteCommandParams
+---@field open? boolean
+---@field handler? lsp.Handler
+
+---@param opts LspCommand
+function M.execute(opts)
+	local params = {
+		command = opts.command,
+		arguments = opts.arguments,
+	}
+	if opts.open then
+		require("trouble").open({
+			mode = "lsp_command",
+			params = params,
+		})
+	else
+		return vim.lsp.buf_request(0, "workspace/executeCommand", params, opts.handler)
+	end
+end
+
+M.action = setmetatable({}, {
+	__index = function(_, action)
+		return function()
+			vim.lsp.buf.code_action({
+				apply = true,
+				context = {
+					only = { action },
+					diagnostics = {},
+				},
+			})
+		end
+	end,
+})
+
 function M.map(bufnr, keys, func, desc, mode)
 	mode = mode or "n"
 	vim.keymap.set(mode, keys, func, {
