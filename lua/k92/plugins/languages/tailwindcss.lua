c2fd4fb11c5279d09bc735cc7b5d7db9fda13e9f
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-03-30 00:47:22 +0800

    fix(nvim.tailwind-autosort): lazy load when `tailwindcss` attached (#632)

--- lua/k92/plugins/languages/tailwindcss.lua
+++ lua/k92/plugins/languages/tailwindcss.lua
@@ -15,8 +15,20 @@ return {
 		"y3owk1n/tailwind-autosort.nvim",
 		-- dir = "~/Dev/tailwind-autosort.nvim", -- Your path
 		-- version = "*",
-		event = "VeryLazy",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		init = function()
+			local augroup = require("k92.utils.autocmds").augroup
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = augroup("lsp_tailwind-autosort_attach"),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client and client.name == "tailwindcss" then
+						require("lazy").load({ plugins = { "tailwind-autosort.nvim" } })
+					end
+				end,
+			})
+		end,
 		---@module "tailwind-autosort"
 		---@type TailwindAutoSort.Config
 		opts = {},
