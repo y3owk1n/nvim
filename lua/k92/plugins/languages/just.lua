1ebe61ad45c915d358a8ae3c179a3d3ca09c8b4c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-04-13 12:52:11 +0800

    feat(nvim.lsp): add just lspconfig (#686)

--- lua/k92/plugins/languages/just.lua
+++ lua/k92/plugins/languages/just.lua
@@ -1,5 +1,7 @@
 local _table = require("k92.utils.table")
 
+vim.lsp.enable("just")
+
 ---@type LazySpec
 return {
 	{
@@ -16,6 +18,13 @@ return {
 					[".justfile"] = "just",
 				},
 			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			_table.add_unique_items(opts.ensure_installed, { "just-lsp" })
 		end,
 	},
 }
