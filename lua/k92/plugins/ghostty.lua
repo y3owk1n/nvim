f0ca31e2fadfe061dcd9343e8d8af973776847bb
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-04-14 17:23:43 +0800

    fix(nvim): remove ghostty highlights (#704)

--- lua/k92/plugins/ghostty.lua
+++
@@ -1,7 +0,0 @@
----@type LazySpec
-return {
-	"ghostty",
-	dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
-	ft = { "ghostty" },
-	enabled = vim.fn.executable("ghostty") == 1,
-}
