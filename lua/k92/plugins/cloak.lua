a37894192f47d31787110591c1dd120b7e29ef61
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-04-14 16:49:29 +0800

    fix(nvim.cloak): update opts (#701)

--- lua/k92/plugins/cloak.lua
+++ lua/k92/plugins/cloak.lua
@@ -1,21 +1,7 @@
 ---@type LazySpec
 return {
 	"laytan/cloak.nvim",
-	event = { "VeryLazy" },
-	opts = {
-		enabled = true,
-		cloak_character = "*",
-		highlight_group = "Comment",
-		cloak_telescope = true,
-		patterns = {
-			{
-				file_pattern = {
-					".env*",
-					"wrangler.toml",
-					".dev.vars",
-				},
-				cloak_pattern = "=.+",
-			},
-		},
-	},
+	event = { "BufReadPre", "BufNewFile" },
+	cmd = { "CloakDisable", "CloakEnable", "CloakToggle" },
+	opts = {},
 }
