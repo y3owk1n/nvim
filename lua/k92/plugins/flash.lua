09c5ccef8426b6b9a002f9fda0d80d4a678c37e3
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-04-16 16:24:08 +0800

    feat(nvim.flash): add `undo-glow` highlights for flash jump (#722)

--- lua/k92/plugins/flash.lua
+++ lua/k92/plugins/flash.lua
@@ -10,7 +10,18 @@ return {
 				"s",
 				mode = { "n", "x", "o" },
 				function()
+					vim.g.ug_ignore_cursor_moved = true
 					require("flash").jump()
+
+					vim.defer_fn(function()
+						local region = require("undo-glow.utils").get_current_cursor_row()
+
+						local undo_glow_opts = require("undo-glow.utils").merge_command_opts("UgSearch", {
+							force_edge = true,
+						})
+
+						require("undo-glow").highlight_region(vim.tbl_extend("force", undo_glow_opts, region))
+					end, 5)
 				end,
 				desc = "Flash",
 			},
