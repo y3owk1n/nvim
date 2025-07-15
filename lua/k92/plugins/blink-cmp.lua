bbd0de36bcab484ef7a2ba41bed3482dcccd54ee
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-07-15 17:27:29 +0800

    fix(nvim): update `blink-cmp` configurations (#938)

--- lua/k92/plugins/blink-cmp.lua
+++ lua/k92/plugins/blink-cmp.lua
@@ -36,7 +36,6 @@ return {
 			},
 			cmdline = {
 				enabled = false,
-				sources = {},
 			},
 			completion = {
 				list = {
@@ -47,23 +46,36 @@ return {
 				menu = {
 					draw = {
 						treesitter = { "lsp" },
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+									return kind_icon
+								end,
+								-- (optional) use highlights from mini.icons
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+							kind = {
+								-- (optional) use highlights from mini.icons
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+						},
 					},
-					border = "rounded",
 				},
 				documentation = {
 					auto_show = true,
 					auto_show_delay_ms = 200,
-					window = {
-						border = "rounded",
-					},
 				},
 			},
 
 			signature = {
 				enabled = true,
-				window = {
-					border = "rounded",
-				},
 			},
 		},
 		-- allows extending the providers array elsewhere in your config
@@ -77,42 +89,12 @@ return {
 			local colors = require("catppuccin.palettes").get_palette()
 
 			local highlights = {
-				-- TODO: Remove this when merged -> https://github.com/catppuccin/nvim/pull/809/files
 				BlinkCmpLabel = { fg = colors.overlay2 },
-				BlinkCmpLabelDeprecated = { fg = colors.overlay0, style = { "strikethrough" } },
-				BlinkCmpKind = { fg = colors.blue },
 				BlinkCmpMenu = { fg = colors.text },
 				BlinkCmpMenuBorder = { fg = colors.blue },
+				BlinkCmpDoc = { fg = colors.overlay2 },
 				BlinkCmpDocBorder = { fg = colors.blue },
-				BlinkCmpSignatureHelpActiveParameter = { fg = colors.mauve },
 				BlinkCmpSignatureHelpBorder = { fg = colors.blue },
-				BlinkCmpLabelMatch = { fg = colors.text, style = { "bold" } },
-				BlinkCmpKindText = { fg = colors.green },
-				BlinkCmpKindMethod = { fg = colors.blue },
-				BlinkCmpKindFunction = { fg = colors.blue },
-				BlinkCmpKindConstructor = { fg = colors.blue },
-				BlinkCmpKindField = { fg = colors.green },
-				BlinkCmpKindVariable = { fg = colors.flamingo },
-				BlinkCmpKindClass = { fg = colors.yellow },
-				BlinkCmpKindInterface = { fg = colors.yellow },
-				BlinkCmpKindModule = { fg = colors.blue },
-				BlinkCmpKindProperty = { fg = colors.blue },
-				BlinkCmpKindUnit = { fg = colors.green },
-				BlinkCmpKindValue = { fg = colors.peach },
-				BlinkCmpKindEnum = { fg = colors.yellow },
-				BlinkCmpKindKeyword = { fg = colors.mauve },
-				BlinkCmpKindSnippet = { fg = colors.flamingo },
-				BlinkCmpKindColor = { fg = colors.red },
-				BlinkCmpKindFile = { fg = colors.blue },
-				BlinkCmpKindReference = { fg = colors.red },
-				BlinkCmpKindFolder = { fg = colors.blue },
-				BlinkCmpKindEnumMember = { fg = colors.teal },
-				BlinkCmpKindConstant = { fg = colors.peach },
-				BlinkCmpKindStruct = { fg = colors.blue },
-				BlinkCmpKindEvent = { fg = colors.blue },
-				BlinkCmpKindOperator = { fg = colors.sky },
-				BlinkCmpKindTypeParameter = { fg = colors.maroon },
-				BlinkCmpKindCopilot = { fg = colors.teal },
 			}
 
 			opts.custom_highlights = opts.custom_highlights or {}
@@ -122,7 +104,9 @@ return {
 			end
 
 			opts.integrations = {
-				blink_cmp = true,
+				blink_cmp = {
+					style = "bordered",
+				},
 			}
 		end,
 	},
