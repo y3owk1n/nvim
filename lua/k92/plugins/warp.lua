864b3a33a898ef619edf8fb9731f8ea316c26d0d
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-07-29 00:09:41 +0800

    fix(nvim): nicer warp window (#991)

--- lua/k92/plugins/warp.lua
+++ lua/k92/plugins/warp.lua
@@ -20,6 +20,26 @@ return {
         split_horizontal = { "-" },
         split_vertical = { "\\" },
       },
+      window = {
+        list = function(lines)
+          -- get all the line widths
+          local line_widths = vim.tbl_map(vim.fn.strdisplaywidth, lines)
+          -- set the width te either the max width or at least 30 characters
+          local max_width = math.max(math.max(unpack(line_widths)), 30)
+          -- set the height to if the number of lines is less than 8 then 8
+          -- otherwise the number of lines
+          local max_height = #lines < 8 and 8 or math.min(#lines, vim.o.lines - 3)
+          -- get the current height of the TUI
+          local nvim_tui_height = vim.api.nvim_list_uis()[1]
+
+          return {
+            width = max_width,
+            height = max_height,
+            row = nvim_tui_height.height - max_height - 4,
+            col = 0,
+          }
+        end,
+      },
     },
     keys = {
       {
