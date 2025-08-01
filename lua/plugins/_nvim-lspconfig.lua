e6a17bc97af78faf3c3b6ddb31e13dea01ec9ab6
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-08-01 08:33:25 +0800

    fix(nvim): add lsp keymaps (#1020)

--- lua/plugins/_nvim-lspconfig.lua
+++ lua/plugins/_nvim-lspconfig.lua
@@ -18,6 +18,13 @@ function M.setup()
     return
   end
 
+  vim.keymap.set("n", "<leader>l", "", { desc = "lsp" })
+  vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "lsp info" })
+  vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<cr>", { desc = "lsp restart" })
+  vim.keymap.set("n", "<leader>ls", "<cmd>LspStart<cr>", { desc = "lsp start" })
+  vim.keymap.set("n", "<leader>ld", "<cmd>LspStop<cr>", { desc = "lsp stop" })
+  vim.keymap.set("n", "<leader>ll", "<cmd>LspLog<cr>", { desc = "lsp log" })
+
   vim.schedule(function()
     require("lsp").init()
   end)
