f9e4faa54e3de2a365e85e64a4e8d3a7db4bb50b
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-08-24 11:28:47 +0800

    feat: remove todo-comments in favor of mini-hipatterns (#1197)

---
+++ lua/k92/plugins/mini-hipatterns.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "echasnovski/mini.hipatterns",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    highlighters = {
+      fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+      hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+      todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+      note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+    },
+  },
+}
